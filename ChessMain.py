cab5139f3de143bf4f4f261bea8d27bfd57fd97e
unknown <shourya.s@gmail.com>
2022-10-25 01:54:15 -0400

    adding new piece images to the board

--- ChessMain.py
+++ ChessMain.py
@@ -8,7 +8,7 @@ DIMENSION = 8
 Square_Size = HEIGHT // DIMENSION
 
 Max_FPS = 15
-Images = {}
+IMAGES = {}
 
 
 ''' TO DO: ADD IMAGES TO THE REPO SO THIS FUNCTION CAN BE CALLED. WILL THROW  UP AN ERROR IF CALLED IN CURRENT STATE '''
@@ -16,7 +16,7 @@ def load_Images():
 
     list_Pieces = ['wp','wR','wN','wB','wQ','wK','bp','bR','bN','bB','bQ','bK']
     for piece in list_Pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/"+piece+".png"), (Square_size,Square_Size))
+        IMAGES[piece] = p.transform.scale(p.image.load("images/images/"+piece+".png"), (Square_Size,Square_Size))
 
 
 def main():
@@ -24,7 +24,7 @@ def main():
     clock = p.time.Clock()
 
     gs = engine.GameState()
-    # load_images() 
+    load_Images() 
 
     running = True
     while running:
@@ -42,15 +42,20 @@ def draw_Game_State(screen, gs):
     draw_Pieces(screen,gs.board)
 
 def draw_Board(screen):
-    colors = [p.Color("white"), p.Color("gray")]
+    colors = [p.Color("white"), p.Color("dark green")]
 
     for r in range(DIMENSION):
         for c in range(DIMENSION):
            color = colors[ ( (r+c)%2 )] 
            p.draw.rect(screen,color, p.Rect(c*Square_Size, r*Square_Size, Square_Size, Square_Size) )
 
-def draw_Pieces(screen,gs):
-    pass
+def draw_Pieces(screen,board):
+
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            piece = board[row][col]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(col*Square_Size, row*Square_Size, Square_Size, Square_Size))
 
 if __name__ == "__main__":
     main()
