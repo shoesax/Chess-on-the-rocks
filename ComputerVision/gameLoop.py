c648a709260ce819c04d307a3586c386c672af9f
unknown <shourya.s@gmail.com>
2023-03-03 20:15:58 -0500

    adding sw vs hw moves

---
+++ ComputerVision/gameLoop.py
@@ -0,0 +1,19 @@
+import cv2
+import Camera
+import Processing
+import Movement
+
+game_over = False
+
+def board_init():
+
+def hardware_move():
+
+def software_move():
+
+def take_picture():
+    
+
+
+while(not game_over):
+
